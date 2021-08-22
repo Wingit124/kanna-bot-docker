9b8e927eae10a950e73a8d8fc618fd7f8694709b
Wingit124 <win9it124@gmail.com>
2021-08-22 16:39:02 +0900

    使用するAPIをRestAPIに変更

--- anime/anime.py
+++ anime/anime.py
@@ -1,35 +1,47 @@
 import requests
 import discord
-from anime.const import Const
+import urllib.parse
 from discord import embeds
 
 
 class Anime:
 
     TOKEN = 'X6nbfeLPhNdGZ8_uWn7PSSXFh4xkwxeT2P_igZUNjmo'
+    json = ''
 
     def get_anime_by_title(self, title):
-        headers = {
-        'Authorization': 'bearer {0}'.format(self.TOKEN),
-        }
-        data = {
-        'query': Const.query_by_title % (title)
-        }
-        response = requests.post('https://api.annict.com/graphql', headers=headers, data=data)
-        json = response.json()
-        node = json['data']['searchWorks']['edges'][0]['node']
-        embed = discord.Embed(title = node['title'], url = node['officialSiteUrl'])
-        embed.set_thumbnail(url=node['image']['recommendedImageUrl'] or '')
-        for cast in node['casts']['edges']:
-            embed.add_field(name=cast['node']['character']['name'], value=cast['node']['name'], inline=True)
+        query = {
+                'access_token': self.TOKEN,
+                'filter_title': title
+                }
+        query = urllib.parse.urlencode(query)
+        response = requests.get('https://api.annict.com/v1/works?' + query)
+        self.json = response.json()
+        work = self.json['works'][0]
+        title = work['title'] or ''
+        title_en = work['title_en'] or ''
+        media_text = work['media_text'] or ''
+        official_site_url = work['official_site_url'] or ''
+        wikipedia_url = work['wikipedia_url'] or ''
+        twitter_username = work['twitter_username'] or ''
+        recommended_url = work['images']['recommended_url'] or ''
+        episodes_count = work['episodes_count'] or ''
+        season_name_text = work['season_name_text'] or ''
 
-        #for anime in json['data']['searchWorks']['edges']:
-            #print('タイトル: {0}'.format(anime['node']['title']))
-            #print('サイトURL: {0}'.format(anime['node']['officialSiteUrl']))
-            #print('画像URL: {0}'.format(anime['node']['image']['recommendedImageUrl'] or ''))
-            #for cast in anime['node']['casts']['edges']:
-                #print('{0}(CV. {1})'.format(cast['node']['character']['name'], cast['node']['name']))
-
-            #print('\n\n\n')
-
+        embed = discord.Embed(title=title, description=title_en)
+        if official_site_url:
+            embed.add_field(name='公式サイト', value='[こちら]({0})'.format(official_site_url), inline=True)
+        if wikipedia_url:
+            embed.add_field(name='Wikipedia', value='[こちら]({0})'.format(wikipedia_url), inline=True)
+        if twitter_username:
+            embed.add_field(name='Twitter', value='[@{0}](https://twitter.com/{0})'.format(twitter_username), inline=True)
+        if episodes_count:
+            embed.add_field(name='エピソード数', value='全{0}話'.format(episodes_count), inline=True)
+        if season_name_text:
+            embed.add_field(name='シーズン', value=season_name_text, inline=True)
+        if media_text:
+            embed.add_field(name='種別', value=media_text, inline=True)
+        if recommended_url:
+            embed.set_image(url=recommended_url)
+        
         return embed
--- anime/anime_cog.py
+++ anime/anime_cog.py
@@ -6,13 +6,17 @@ import requests
 
 class AnimeCog(commands.Cog):
 
+    log = {}
+
     # 初期化処理
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='search_anime', aliases=['sa'])
     async def search_anime(self, context, title):
-        await context.send(embed=Anime().get_anime_by_title(title))
+        anime = Anime()
+        message = await context.send(embed=anime.get_anime_by_title(title))
+        self.log[message.id] = anime
 
 def setup(bot):
     return bot.add_cog(AnimeCog(bot))
