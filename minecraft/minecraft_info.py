bc5a7ddd68a3142da19bd2b682eaa21f9dd99b43
win9y <win9it.124@gmail.com>
2023-07-28 16:24:45 +0900

    add: インスタンス料金を表示できるように

--- minecraft/minecraft_info.py
+++ minecraft/minecraft_info.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import datetime
+import json
 from discord.embeds import Embed
 
 
@@ -22,10 +23,12 @@ class MinecraftInfo:
     output_embed: Embed
 
     instance: any
+    pricing: any
     status: str
 
     def __init__(self):
         self.connect()
+        self.connect_pricing()
 
     def connect(self):
         self.instance = boto3.resource(
@@ -34,16 +37,61 @@ class MinecraftInfo:
             aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY,
             region_name=self.AWS_REGION_NAME
         ).Instance(self.AWS_INSTANCE_ID)
+    
+    def connect_pricing(self):
+        self.pricing = boto3.client("pricing",
+            aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY,
+            region_name='us-east-1'
+        )
+    
+    def fetch_price(self, instance_type: str) -> float:
+        response = self.pricing.get_products(
+            ServiceCode='AmazonEC2',
+            Filters=[
+              {
+                'Type': 'TERM_MATCH',
+                'Field': 'instanceType',
+                'Value': instance_type
+              },
+              {
+                  'Type': 'TERM_MATCH',
+                  'Field': 'termType',
+                  'Value': 'OnDemand'
+              },
+              {
+                  'Type': 'TERM_MATCH',
+                  'Field': 'location',
+                  'Value': 'Asia Pacific (Tokyo)'
+              },
+              {
+                  'Type': 'TERM_MATCH',
+                  'Field': 'operatingSystem',
+                  'Value': 'Linux'
+              }
+            ],
+            MaxResults=1
+        )
+        response = json.loads(response['PriceList'][0])
+        price_per_hour = 0
+        for v in response['terms']['OnDemand'].values():
+            for v2 in v['priceDimensions'].values():
+                price_per_hour = float(v2['pricePerUnit']['USD'])
+        return price_per_hour
 
     def fetch(self) -> Embed:
         self.connect()
         self.status = self.instance.state['Name']
         ipv4 = self.instance.public_ip_address
+        instance_type = self.instance.instance_type
         embed: Embed = Embed(title='Minecraftサーバー情報', color=0x00b0f4, timestamp=datetime.datetime.utcnow())
         embed.add_field(name='状態', value=self.STATUS_DICT[self.status], inline=True)
         if ipv4:
             embed.add_field(name='IPv4', value=ipv4, inline=True)
-            embed.add_field(name='Dynmap', value='[こちら](http://{0}:8123)'.format(ipv4), inline=False)
+            embed.add_field(name='Dynmap', value='[こちら](http://{0}:8123)'.format(ipv4), inline=True)
+        if instance_type:
+            embed.add_field(name='インスタンスタイプ', value=instance_type, inline=True)
+            embed.add_field(name='料金', value='{0}USD/時'.format(self.fetch_price(instance_type)), inline=True)
         embed.set_footer(text='いたずらしないでね;o;')
         return embed
     
@@ -55,7 +103,7 @@ class MinecraftInfo:
                 embed = self.fetch()
             except:
                 embed = self.fetch()
-                embed.add_field(name='エラーメッセージ', value='サーバーの起動に失敗したよ。少し時間をおいてからもう一度ためしてね。', inline=False)
+                embed.add_field(name='エラーメッセージ', value='サーバーの起動に失敗しました。\n少し時間をおいてもう一度お試しください。', inline=False)
         else:
             embed = self.fetch()
             embed.add_field(name='エラーメッセージ', value='サーバーの起動はステータスが停止済みでないと行えません。', inline=False)
